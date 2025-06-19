f02b78c26d51d55d8ceeb2a9e53542df593399bc
wonCha25 <chawj1234@gmail.com>
2025-06-19 22:54:25 +0900

    feat: reCreate hotMenuBar Project

---
+++ hotMenuBar/hotMenuBar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  hotMenuBar
+//
+//  Created by 차원준 on 6/19/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ hotMenuBar/hotMenuBar/hotMenuBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  hotMenuBarApp.swift
+//  hotMenuBar
+//
+//  Created by 차원준 on 6/19/25.
+//
+
+import SwiftUI
+
+@main
+struct hotMenuBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
